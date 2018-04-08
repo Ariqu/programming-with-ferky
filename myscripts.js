c3288a1173fcc23af98a4da533005b7658d19275
vAriquu <ef3ko1@gmail.com>
2018-04-08 18:29:27 +0200

    programming-add styles and first try of js

--- myscripts.js
+++ myscripts.js
@@ -1,1 +1,12 @@
-console.log('siema tu kuba');
+
+
+  var element = document.getElementById("p-tag-clickable")
+
+
+  if (element) {
+    element.addEventListener("click", myFunction);
+  }
+
+  function myFunction() {
+    console.log('siema to kuba wy chore dziki');
+  }
