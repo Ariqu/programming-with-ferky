fdbc9e81a51e769026d6a354e86dfbd8b8b25f55
vAriquu <ef3ko1@gmail.com>
2018-04-11 16:21:28 +0200

    ADD EXAMPLE MENU AND SOON JS

--- myscripts.js
+++ myscripts.js
@@ -5,7 +5,7 @@
 
   if (element) {
     element.addEventListener("click", myFunction);
-  }
+-
 
   function myFunction() {
     console.log('siema to kuba wy chore dziki');
@@ -13,7 +13,4 @@
   function ne(o){
      if(document.getElementById(o).style.display=='') document.getElementById(o).style.display = 'none';
      else document.getElementById(o).style.display='';
-}
-{
-alert("HELLO!!")
 }
